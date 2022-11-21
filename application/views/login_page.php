2fb373b545d13bef5a7a0fa612dcb7a2d556017c
isverse31 <darioreyea974@gmail.com>
2022-11-21 17:14:51 -0600

    cambios

--- application/views/login_page.php
+++ application/views/login_page.php
@@ -1,51 +1,81 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<title>CodeIgniter Login</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingreso de Usuarios</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+
 </head>
+
 <body>
-<div class="container">
-	<h1 class="page-header text-center">Bienvenido</h1>
-	<div class="row">
-		<div class="col-sm-4 col-sm-offset-4">
-			<div class="login-panel panel panel-primary">
-		        <div class="panel-heading">
-		            <h3 class="panel-title"><span class="glyphicon glyphicon-lock"></span> Login
-		            </h3>
-		        </div>
-		    	<div class="panel-body">
-		        	<form method="POST" action="<?php echo base_url(); ?>index.php/user/login">
-		            	<fieldset>
-		                	<div class="form-group">
-		                    	<input class="form-control" placeholder="Email" type="email" name="CorreoElectronico" required>
-		                	</div>
-		                	<div class="form-group">
-		                    	<input class="form-control" placeholder="Password" type="password" name="Contrasenia" required>
-		                	</div>
-		                	<button type="submit" class="btn btn-lg btn-primary btn-block"><span class="glyphicon glyphicon-log-in"></span> Login</button>
-		            	</fieldset>
-		        	</form>
-		    	</div>
-		    </div>
-			<div class="pie-form">
-                        <a href="#">¿Perdiste tu contraseña?</a>
-                        <a href="index.php/RegistrosC/FormularioRegistros">¿No tienes Cuenta? Registrate</a>
-                        <hr>
-                        <a href="#">« Volver</a>
-                    </div>
-			<?php
+
+    <section class="left-section">
+        <div id="left-cover" class="cover cover-hide">
+            <img src="img/cover.png" alt="">
+            <h1>Bienvenido !</h1>
+            <h3>Actualmente tienes una cuenta ?</h3>
+            <button type="button" class="switch-btn" onclick="location.reload()">Login</button>
+            <a href="index.php/CambioPass/CambioPassV">Se te olvido la contraseña?</a>
+        </div>
+        <div id="left-form" class="form fade-in-element">
+
+
+            <form action="<?php echo base_url(); ?>index.php/user/login" method="post" class="elementor-element elementor-element-953c566 animated-fast elementor-widget elementor-widget-text-editor animated fadeInUp">
+                <center><img src="img/logo1.jpeg" redonded alt="" width="260"></center>
+                <input type="email" name="CorreoElectronico" class="input-box" placeholder="Correo" required>
+                <input type="password" name="Contrasenia" class="input-box" placeholder="Contraseña" required>
+                <input type="submit" name="login-btn" class="btn" value="Ingresar">
+            </form>
+
+
+            <?php
 				if($this->session->flashdata('error')){
 					?>
-					<div class="alert alert-danger text-center" style="margin-top:20px;">
-						<?php echo $this->session->flashdata('error'); ?>
-					</div>
-					<?php
+            <div class="alert alert-danger text-center" style="margin-top:20px;">
+                <?php echo $this->session->flashdata('error'); ?>
+            </div>
+            <?php
 				}
 			?>
-		</div>
-	</div>
-</div>
+            <a href="" right="50px">¿Se te olvido la contraseña?</a>
+        </div>
+
+    </section>
+
+    <section class="right-section">
+        <div id="right-cover" class="cover fade-in-element">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPyrWju5ipIipYD8-RxAzHyqSFGrtwZIbh53mW0dlZXCOyk7ZZLzgezHf4o11Zdd7X2f4&usqp=CAU"
+                alt="">
+            <h1>Bienvenido !</h1>
+            <h3><b> No tienes una cuenta ?</b></h3>
+            <button type="button" class="switch-btn" onclick="switchSignup()">Crear Cuenta</button>
+        </div>
+        <div id="right-form" class="form form-hide">
+            <h1>Alta de Usuario</h1>
+            <form action="<?=base_url('index.php/RegistrosC/FormularioRegistros')?>" method="post">
+                <input type="text" name="Nombre" class="input-box" placeholder="Nombre" required>
+                <input type="text" name="ApellidoPaterno" class="input-box" placeholder="Primer Apellido" required>
+                <input type="text" name="ApellidoMaterno" class="input-box" placeholder="Segundo Apellido" required>
+                <input type="email" name="CorreoElectronico" class="input-box" placeholder="Correo" required>
+                <input type="password" name="Contrasenia" class="input-box" placeholder="Contraseña" required>
+                <input type="submit" name="signup-btn" class="btn" value="Aceptar">
+            </form>
+            <?php
+				if($this->session->flashdata('true')){
+					?>
+            <div class="alert alert-danger text-center" style="margin-top:20px;">
+                <?php echo $tempdata = array('newuser' => TRUE, 'message' => 'Thanks for joining!'); ?>
+            </div>
+            <?php
+				}
+			?>
+        </div>
+    </section>
+
+    <script src="js/main.js"></script>
+
 </body>
+
 </html>
